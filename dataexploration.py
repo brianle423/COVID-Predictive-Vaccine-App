59136d4ed6e6ddb53341b98da24f100530a098ac
brianle423 <75450058+brianle423@users.noreply.github.com>
2021-04-15 13:45:47 -0700

    Add files via upload

---
+++ dataexploration.py
@@ -0,0 +1,9 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn import linear_model
+import warnings
+warnings.filterwarnings('ignore')
+
